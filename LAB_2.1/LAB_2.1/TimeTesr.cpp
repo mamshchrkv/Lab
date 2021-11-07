8c9a7951f85b814e38f2efa83ac54c59b3a3ca28
mamshchrkv <84730638+mamshchrkv@users.noreply.github.com>
2021-11-07 21:14:47 +0300

    Update TimeTesr.cpp

--- LAB_2.1/LAB_2.1/TimeTesr.cpp
+++ LAB_2.1/LAB_2.1/TimeTesr.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-    //time_array_int();
-    //time_array_float();
-    //time_list_int();
-    //time_list_float();
+    time_array_int();
+    time_array_float();
+    time_list_int();
+    time_list_float();
     //time_array_int_sort();
     //time_array_float_sort();
     //time_list_int_sort();
-    time_list_float_sort();
+    //time_list_float_sort();
     cout << "Done!";
     return 0;
 }
