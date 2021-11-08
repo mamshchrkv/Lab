af474a27b36adac03c6d540b66e122922dc8305e
mamshchrkv <84730638+mamshchrkv@users.noreply.github.com>
2021-11-08 20:31:32 +0300

    Update SequenceMake.h

--- LAB_2.1/LAB_2.1/SequenceMake.h
+++ LAB_2.1/LAB_2.1/SequenceMake.h
@@ -4,10 +4,11 @@ T* makeSequenceRand(int dimension) {
     srand(500);
     T* memory = new T[dimension];
     for (int i = 0; i < dimension; i++) {
-        memory[i] = rand() % 100;
+        memory[i] = rand() % 1000;
     }
     return memory;
 }
+
 template<typename T>
 T* makeSequence(int dimension) {
     cout << "Fill the Sequence" << "\n";
@@ -16,6 +17,26 @@ T* makeSequence(int dimension) {
         T el;
         cin >> el;
         memory[i] = el;
+    }
+    return memory;
+}
+
+template<typename T>
+T* makeSequenceSortEnd(int dimension) {
+    cout << "Fill the Sequence" << "\n";
+    T* memory = new T[dimension];
+    for (int i = 0; i < dimension; i++) {
+        memory[i] = dimension - i;
+    }
+    return memory;
+}
+
+template<typename T>
+T* makeSequenceSort(int dimension) {
+    cout << "Fill the Sequence" << "\n";
+    T* memory = new T[dimension];
+    for (int i = 0; i < dimension; i++) {
+        memory[i] = i;
     }
     return memory;
 }
